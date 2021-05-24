3c85b5172d775af8bda06d0cd6cee8a521785b26
Wojtek Sowinski <ws420@ic.ac.uk>
2021-05-24 22:25:59 +0100

    Implemented multiply instructions

---
+++ src/multiply.c
@@ -0,0 +1,28 @@
+#include "insttypes.h"
+#include "executefuncs.h"
+
+StatusCode m_execute(State *state) {
+
+    MInst params = state->decoded.inst.m;
+    uint m = state->registers[params.Rm];
+    uint s = state->registers[params.Rs];
+
+    //Calculate the result
+    uint d = m * s;
+    if (params.bits_ipuasl & BIT_A) {
+        uint n = state->registers[params.Rn];
+        d += n;
+    }
+
+    //Write the result to Rd
+    state->registers[params.Rd] = d;
+
+    //Update CPSR
+    if (params.bits_ipuasl & BIT_S) {
+        uint N = d & (1u << 31u);
+        uint Z = (d == 0u) << 30u;
+        state->CPSR = N | Z | (state->CPSR & 0x3FFFFFFF);
+    }
+
+    return CONTINUE;
+}
