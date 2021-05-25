b1d1ad6187a7a0591ac1686395869cbf2fa9872e
ffa20 <ffa20@ic.ac.uk>
2021-05-25 17:21:16 +0100

    Removed all 0b constants from decoder.c and replaced some bits with helper function calls.

--- src/decoder.c
+++ src/decoder.c
@@ -1,29 +1,31 @@
 #include "insttypes.h"
+#include "helpers.h"
 #include "decoder.h"
 
 #include <string.h>
 
-#define BYTEMASK 0b11111111u
-#define TYPEMASK (0b111111u << 22u)
+#define BYTEMASK      255u
 
-#define RndMASK (0b1111u << 16u)
-#define RdnMASK (0b1111u << 12u)
-#define RsMASK  (0b1111u << 8u)
-#define RmMASK  (0b1111u)
+#define TYPEMASK      ((1u << 7u) - 1u)
+#define TYPEOFFSET    22u
+
+#define RndMASK       (15u << 16u)
+#define RdnMASK       (15u << 12u)
+#define RsMASK        (15u << 8u)
+#define RmMASK        (15u)
+
+#define BRANCHPATTERN (5u << 3u)
+#define BRANCHMASK    (15u)
+
+#define SDTPATTERN    (1u << 4u)
+#define SDTMASK       (49u)
+
+#define DPMASK        (3u << 4u)
 
 StatusCode decode(State *state) {
     // First, convert the instruction to BE.
     uint raw = state->fetched;
-    uint translated = 0u;
-
-    // Use bitmasking to get to where we need to get.
-    for (uint i = 3u; i <= 3u; --i) {
-        uint mask = BYTEMASK << (8u * i);
-        uint masked = raw & mask;
-
-        uint inv3 = (3u - i);
-        translated |= (masked << (8u * inv3));
-    }
+    uint translated = swap_endianness(raw);
 
     // Clear out decoded instruction.
     memset(&(state->decoded), 0, sizeof(__InstUnion__));
@@ -32,8 +34,8 @@ StatusCode decode(State *state) {
     state->decoded.condition = translated >> 28u;
 
     // Detect instruction type
-    uint typemasked = (translated & TYPEMASK) >> 24u;
-    if ((typemasked & 0b111100u) == 0b101000u) {
+    uint typemasked = select_bits(translated, TYPEMASK, TYPEOFFSET, true);
+    if (select_bits(typemasked, BRANCHMASK, 0u, false) == BRANCHPATTERN) {
         // Branch
         state->decoded.type = B;
         state->decoded.inst.b.offset = translated & ((1u << 24u) - 1u);
@@ -46,7 +48,7 @@ StatusCode decode(State *state) {
         state->decoded.inst.m.Rn = (translated & RdnMASK) >> 12u;
         state->decoded.inst.m.Rs = (translated & RsMASK) >> 8u;
         state->decoded.inst.m.Rm = (translated & RmMASK);
-    } else if ((typemasked & 0b110001) == 0b010000u) {
+    } else if (select_bits(typemasked, SDTMASK, 0u, false) == SDTPATTERN) {
         // Single Data Transfer
         state->decoded.type = SDT;
         state->decoded.inst.sdt.bits_ipuasl = translated & BITS_IPUASL;
@@ -55,7 +57,7 @@ StatusCode decode(State *state) {
         state->decoded.inst.sdt.Rd = (translated & RdnMASK) >> 12u;
 
         state->decoded.inst.sdt.offset = translated & OPR2_OR_OFFSET;
-    } else if ((typemasked & 0b110000) == 0) {
+    } else if (select_bits(typemasked, DPMASK, 0u, false) == 0u) {
         // Data Processing
         state->decoded.type = DP;
         state->decoded.inst.dp.bits_ipuasl = translated & BITS_IPUASL;
