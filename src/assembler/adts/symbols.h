e7e8057a7ec65b19766dfec2fee9709a8473f618
ffa20 <ffa20@ic.ac.uk>
2021-05-29 11:44:31 +0100

    Moved mnemonics to own parser-related header file.

--- src/assembler/adts/symbols.h
+++ src/assembler/adts/symbols.h
@@ -6,38 +6,8 @@
 
 #include "../../helpers/insttypes.h"
 
-// Macros for use in parser:
-#define MAXIMUM_LINE_LENGTH   512
+// Macros for use in this file:
 #define MAXIMUM_SYMBOL_LENGTH 120
-
-/**
- * The different mnemonics used in the assembly.
- */
-typedef enum {
-    asm_add,  /**< DP: add */
-    asm_sub,  /**< DP: sub */
-    asm_rsb,  /**< DP: reverse sub */
-    asm_and,  /**< DP: logical AND */
-    asm_eor,  /**< DP: logical XOR */
-    asm_orr,  /**< DP: logical OR */
-    asm_mov,  /**< DP: move to register */
-    asm_tst,  /**< DP: test bits */
-    asm_teq,  /**< DP: test bitwise equality */
-    asm_cmp,  /**< DP: compare */
-    asm_mul,  /**< M: multiply */
-    asm_mla,  /**< M: multiply accumulate */
-    asm_ldr,  /**< SDT: load register */
-    asm_str,  /**< SDT: store register */
-    asm_beq,  /**< B: branch if == */
-    asm_bne,  /**< B: branch if != */
-    asm_bge,  /**< B: branch if >= */
-    asm_blt,  /**< B: branch if < */
-    asm_bgt,  /**< B: branch if > */
-    asm_ble,  /**< B: branch if <= */
-    asm_b,    /**< B: unconditional branch */
-    asm_lsl,  /**< SPECIAL: left shift */
-    asm_andeq /**< SPECIAL: halt */
-} Mnemonic;
 
 /**
  * A symbol-address associative pair.
