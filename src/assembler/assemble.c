e4d4b0a531304281bb9e3e077b3536bcec179c62
Edward Hartley <edward.hartley20@imperial.ac.uk>
2021-05-31 11:44:33 +0100

    unfinished attempt at assemble.c

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -1,5 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#include "parser/symbols.h"
+#include "parser/parser.h"
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+
+    // Open assembly file.
+    assert(argc > 1);
+    FILE *file = fopen(argv[1], "r");
+    // If file does not exist, it has not been opened successfully.
+    if (!file) {
+       return FILE_OPEN_ERROR;
+    }
+
+    // Completes first pass and rewinds file.
+    SymbolMap* symbolMap = new_symbol_map(0);
+    AssemblyInfo assemblyInfo = collect_symbols(symbolMap, file);
+
+
+    translate(symbolMap, file);
+    
+
+
+
+
+
+    return EXIT_SUCCESS;
 }
