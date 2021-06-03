7bd500709d5ee448d60965ec5cbe575203830792
ffa20 <ffa20@ic.ac.uk>
2021-06-03 15:43:31 +0100

    Cleaned up assemble.c

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -23,7 +23,7 @@
 
 #include "mainmap.h"
 
-StatusCode translate_into_file(SymbolMap *, FILE *, FILE *, AssemblyInfo *);
+StatusCode translate_into_in_file(SymbolMap *, FILE *, FILE *, AssemblyInfo *);
 void init_translation_map(void);
 
 int main(int argc, char **argv) {
@@ -34,28 +34,26 @@ int main(int argc, char **argv) {
     init_translation_map();
 
     // Open assembly file.
-    FILE *file = fopen(argv[1], "r");
-    // If file does not exist, it has not been opened successfully.
-    if (!file) {
+    FILE *in_file = fopen(argv[1], "r");
+    // If in_file does not exist, it has not been opened successfully.
+    if (!in_file) {
        return FILE_OPEN_ERROR;
     }
 
     // Completes first pass and rewinds file.
-    SymbolMap* symbolMap = new_symbol_map(1);
-    AssemblyInfo assemblyInfo = collect_symbols(symbolMap, file);
+    SymbolMap* symbol_map = new_symbol_map(1);
+    AssemblyInfo assembly_info = collect_symbols(symbol_map, in_file);
 
     // Creates new file and pads it for load immediate instructions
-    FILE *outFile = fopen(argv[2], "wb+");
-    // for( ;ftell(outFile) < assemblyInfo.instructions * 4; ) {
-    //     putc(0u, outFile);
-    // }
-    rewind(outFile);
-    StatusCode code = translate_into_file(symbolMap, file, outFile, &assemblyInfo);
+    FILE *out_file = fopen(argv[2], "wb+");
 
-    fclose(outFile);
-    fclose(file);
+    // Translate assembly into output file.
+    StatusCode code = translate_into_in_file(symbol_map, in_file, out_file, &assembly_info);
 
-    free_symbol_map(symbolMap);
+    fclose(out_file);
+    fclose(in_file);
+
+    free_symbol_map(symbol_map);
     free_symbol_map(translation_map);
 
     status_code_handler(code, NULL);
@@ -71,7 +69,7 @@ void init_translation_map(void) {
     translation_map = new_symbol_map(4);
     assert(translation_map);
 
-    //Data Processing
+    // Data Processing
     add_to_symbol_map(translation_map, "and", dp_and);
     add_to_symbol_map(translation_map, "eor", dp_eor);
     add_to_symbol_map(translation_map, "sub", dp_sub);
@@ -85,7 +83,7 @@ void init_translation_map(void) {
     add_to_symbol_map(translation_map, "andeq", dp_andeq);
     add_to_symbol_map(translation_map, "lsl", dp_lsl);
 
-    //Branch
+    // Branch
     add_to_symbol_map(translation_map, "beq", eq);
     add_to_symbol_map(translation_map, "bne", ne);
     add_to_symbol_map(translation_map, "bge", ge);
@@ -95,11 +93,11 @@ void init_translation_map(void) {
     add_to_symbol_map(translation_map, "bal", al);
     add_to_symbol_map(translation_map, "b",   al);
 
-    //Multiply
+    // Multiply
     add_to_symbol_map(translation_map, "mul", mul);
     add_to_symbol_map(translation_map, "mla", mla);
 
-    //Single Data Transfer
+    // Single Data Transfer
     add_to_symbol_map(translation_map, "ldr", ldr);
     add_to_symbol_map(translation_map, "str", str);
 }
@@ -110,6 +108,7 @@ static const TranslateFunction t_functions[5] = { dp_translate, m_translate, sdt
 // Endianness check (because we need to ensure that the file is *definitely* in little endian).
 // Returns true if big endian.
 bool check_endianness(void) {
+    // Somewhat undefined behaviour is being used here...
     union {
         uint32_t i;
         uchar buf[4];
@@ -119,19 +118,19 @@ bool check_endianness(void) {
     return checker.buf[3];
 }
 
-StatusCode translate_into_file(SymbolMap *symbolMap, FILE* file, FILE* outFile, AssemblyInfo *assemblyInfo) {
+StatusCode translate_into_in_file(SymbolMap *symbol_map,FILE* in_file, FILE* out_file, AssemblyInfo *assembly_info) {
     char line[MAXIMUM_LINE_LENGTH] = { '\0' };
     StatusCode code;
     uint offset = 0;
     bool big_e = check_endianness();
 
-    // Read file line by line.
-    while (!feof(file)) {
+    // Read in_file line by line.
+    while (!feof(in_file)) {
         // tokens set to null in each iteration
         char *tokens[6] = { NULL };
 
         // Read line
-        if (!fgets(line, MAXIMUM_LINE_LENGTH, file)) {
+        if (!fgets(line, MAXIMUM_LINE_LENGTH, in_file)) {
             if (errno) {
                 // Error while reading file.
                 return FILE_READ_ERROR;
@@ -145,30 +144,30 @@ StatusCode translate_into_file(SymbolMap *symbolMap, FILE* file, FILE* outFile, 
 
         if (true_start) {
             // Remove the newline left behind by fgets (if it is there).
-            char *newl = strchr(line, '\n');
-            if (newl) {
-                *newl = '\0';
+            char *nl = strchr(line, '\n');
+            if (nl) {
+                *nl = '\0';
             }
 
-            uint currentOp = 0u;
-            char *savePtr;
+            uint current_op = 0u;
+            char *save_ptr;
 
             // Get first token from line
-            char *token = strtok_r(true_start, " ,", &savePtr);
+            char *token = strtok_r(true_start, " ,", &save_ptr);
 
             // If first token is a label, move onto next line.
             if (strstr(token, ":") != NULL) {
-                // token = strtok_r(NULL, " ,", &savePtr);
+                // token = strtok_r(NULL, " ,", &save_ptr);
                 continue;
             }
 
             // Copy all tokens into array
-            for (int i = 0; token != NULL; token = strtok_r(NULL, " ,", &savePtr)) {
+            for (int i = 0; token != NULL; token = strtok_r(NULL, " ,", &save_ptr)) {
                 tokens[i++] = token;
             }
 
             // Call translate function from array according to the type represented by the first token.
-            code = t_functions[type_from_string(tokens[0])](tokens, symbolMap, offset, &currentOp, assemblyInfo);
+            code = t_functions[type_from_string(tokens[0])](tokens, symbol_map, offset, &current_op, assembly_info);
 
             // Error handling here
             if (code) {
@@ -176,24 +175,26 @@ StatusCode translate_into_file(SymbolMap *symbolMap, FILE* file, FILE* outFile, 
             }
 
             // Write binary to file in little endian byte order.
-            uint binary = big_e ? swap_endianness(currentOp) : currentOp;
-            fwrite(&binary, sizeof(uint), 1, outFile);
+            uint binary = big_e ? swap_endianness(current_op) : current_op;
+            fwrite(&binary, sizeof(uint), 1, out_file);
 
             // If there was a ldr instruction which required a value added to the binary file.
-            if (assemblyInfo->int_to_load) {
+            if (assembly_info->int_to_load) {
                 // Store the current position of the file.
                 fpos_t nextOp;
-                fgetpos(outFile, &nextOp);
+                fgetpos(out_file, &nextOp);
+
                 // Seek the end of the file and save the value there.
-                fseek(outFile, assemblyInfo->instructions * INSTRUCTION_BYTE_LENGTH, SEEK_SET);
-                binary = big_e ? swap_endianness(assemblyInfo->load_int) : assemblyInfo->load_int;
-                fwrite(&binary, sizeof(binary), 1, outFile);
+                fseek(out_file, assembly_info->instructions * INSTRUCTION_BYTE_LENGTH, SEEK_SET);
+                binary = big_e ? swap_endianness(assembly_info->load_int) : assembly_info->load_int;
+                fwrite(&binary, sizeof(binary), 1, out_file);
+
                 // Revert the file pointer to the correct place.
-                fsetpos(outFile, &nextOp);
+                fsetpos(out_file, &nextOp);
 
                 // Update assembly info so the next value is saved in the next space
-                assemblyInfo->int_to_load = false;
-                assemblyInfo->instructions++;
+                assembly_info->int_to_load = false;
+                assembly_info->instructions++;
             }
 
             // Increment offset so it holds the current line number of the file.
@@ -205,7 +206,7 @@ StatusCode translate_into_file(SymbolMap *symbolMap, FILE* file, FILE* outFile, 
     return code;
 }
 
-StatusCode h_translate(char **tokens, SymbolMap *symbols, uint current_offset, uint *output, AssemblyInfo *assemblyInfo) {
+StatusCode h_translate(char **tokens, SymbolMap *symbols, uint current_offset, uint *output, AssemblyInfo *assembly_info) {
     *output = 0u;
     return CONTINUE;
 }
