02ae669bbd7795d16b172ea4646510e3789f66f8
Edward Hartley <edward.hartley20@imperial.ac.uk>
2021-06-03 15:13:22 +0100

    implemented shifted register support in DP translate

--- src/assembler/translateinst/dataprocessing.c
+++ src/assembler/translateinst/dataprocessing.c
@@ -8,22 +8,55 @@ int parse_register(char *register_label) {
     return atoi(register_label + 1);
 }
 
-StatusCode parse_operand2(char *operand_string, uint *output){
-    uint operand = strtol(operand_string + 1, NULL, 0);
-    uint shift = 0;
-    do
-    {
-        if (operand & ~255)
+StatusCode parse_operand2(char **tokens, uint *output){
+    if (!(strstr(tokens[0], "r"))) {
+        uint operand = strtol(tokens[0] + 1, NULL, 0);
+        uint shift = 0;
+        do
         {
-            operand = operand << 2 | operand >> 30u;
-            shift ++;
-        } else {
-            *output |= operand | (shift << 8);
-            return CONTINUE;
+            if (operand & ~255)
+            {
+                operand = operand << 2 | operand >> 30u;
+                shift ++;
+            } else {
+                *output |= operand | (shift << 8);
+                return CONTINUE;
+            }
+        } while (shift < 16);
+        return PARSE_ERROR;
+    }
+    // Operand2 is a shifted register
+    // Collect Rm
+    *output |= parse_register(tokens[0]);
+    if (check_parse_error(output)) {
+        return PARSE_ERROR;
+    }
+
+    // If there is a shift.
+    if (tokens[1]) {
+        // Collect shift type.
+        if (strcmp(tokens[1], "lsl") == 0) {
+            *output |= 0u;
+        } else if (strcmp(tokens[1], "lsr") == 0) {
+            *output |= 1u << 5u;
+        } else if (strcmp(tokens[1], "asr") == 0) {
+            *output |= 2u << 5u;
+        } else if (strcmp(tokens[1], "ror") == 0) {
+            *output |= 3u << 5u;
         }
-    } while (shift < 16);
-    return PARSE_ERROR;
-    //TODO (optional): Add support shifted registers.
+
+        // Collect shift value/register.
+        if (strchr(tokens[2], 'r')) {
+            *output |= parse_register(tokens[2]) << 8u;
+            *output |= 1u << 4u;
+        } else {
+            *output |= (uint) strtol(tokens[2] + 1, NULL, 0) << 7u;
+        }
+        if (check_parse_error(output)) {
+            return PARSE_ERROR;
+        }
+    }
+    return CONTINUE;
 }
 
 StatusCode dp_translate(char **tokens, SymbolMap *symbols, uint current_offset, uint *output, AssemblyInfo *assemblyInfo) {
@@ -39,7 +72,7 @@ StatusCode dp_translate(char **tokens, SymbolMap *symbols, uint current_offset, 
     out |= opcode << 21u;
 
     // Set the I bit if immediate operands used
-    for (char **t = tokens; t < tokens + 6; ++t) {
+    for (char **t = tokens; t < tokens + 4; ++t) {
         if (*t && strchr(*t, '#')) {
             out |= BIT_I;
             break;
@@ -66,16 +99,16 @@ StatusCode dp_translate(char **tokens, SymbolMap *symbols, uint current_offset, 
     case dp_cmp:
         out |= 1u << 20u;   //Set the S bit
         out |= parse_register(tokens[1]) << 16u;
-        if (parse_operand2(tokens[2], &out)) return PARSE_ERROR;
+        if (parse_operand2(tokens + 2, &out)) return PARSE_ERROR;
         break;
     case dp_mov:
         out |= parse_register(tokens[1]) << 12u;
-        if (parse_operand2(tokens[2], &out)) return PARSE_ERROR;
+        if (parse_operand2(tokens + 2, &out)) return PARSE_ERROR;
         break;
-    default:
+    default: // and, eor, orr, add, sub, rsb 
         out |= parse_register(tokens[1]) << 12u;
         out |= parse_register(tokens[2]) << 16u;
-        if (parse_operand2(tokens[3], &out)) return PARSE_ERROR;
+        if (parse_operand2(tokens + 3, &out)) return PARSE_ERROR;
         break;
     }
 
