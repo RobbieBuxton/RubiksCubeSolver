768348a78bd0d2722d9fffe88bb3ebd0960320ad
ffa20 <ffa20@ic.ac.uk>
2021-06-09 19:23:03 +0100

    Need confirmation on something before going further.

--- extension/src/solver/movequeue.c
+++ extension/src/solver/movequeue.c
@@ -1,12 +1,24 @@
 #include "movequeue.h"
 
+static inline size_t left_child(size_t curr) {
+    return (curr << 1u) + 1u;
+}
+
+static inline size_t right_child(size_t curr) {
+    return left_child(curr) + 1u;
+}
+
+static inline size_t parent(size_t curr) {
+    return (curr - 1u) >> 1u;
+}
+
 MovePriorityQueue *new_move_priority_queue(size_t initial_size) {
     MovePriorityQueue *queue = (MovePriorityQueue *) malloc(sizeof(MovePriorityQueue));
     if (!queue) {
         return NULL;
     }
 
-    queue->state_queue = (CubeState *) calloc(initial_size, sizeof(MoveQueueNode));
+    queue->state_queue = (MoveQueueNode *) calloc(initial_size, sizeof(MoveQueueNode));
     if (!queue->state_queue) {
         free(queue);
         return NULL;
@@ -16,5 +28,28 @@ MovePriorityQueue *new_move_priority_queue(size_t initial_size) {
     queue->count = 0u;
 
     return queue;
+}
+
+bool free_move_priority_queue(MovePriorityQueue* queue) {
+    if (!queue) {
+        return false;
+    }
+
+    free(queue->state_queue);
+    free(queue);
+
+    return true;
+}
+
+static void sift_up(size_t start) {
+    if (start <= 0u) {
+        // We are at the
+        return;
+    }
+}
+
+bool add_to_move_priority_queue(MovePriorityQueue queue, CubeState state, uint64_t heuristic_value) {
+    // TODO
+    return false;
 }
 
--- extension/src/solver/movequeue.h
+++ extension/src/solver/movequeue.h
@@ -1,6 +1,8 @@
 #ifndef __MOVEQUEUE_H__
 #define __MOVEQUEUE_H__
 
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -21,6 +23,10 @@ typedef struct {
 } MovePriorityQueue;
 
 MovePriorityQueue *new_move_priority_queue(size_t initial_size);
+
+bool free_move_priority_queue(MovePriorityQueue* queue);
+
+bool add_to_move_priority_queue(MovePriorityQueue queue, CubeState state, uint64_t heuristic_value);
 
 #endif  // __MOVEQUEUE_H__
 
